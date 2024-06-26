33e4b7211b8e384482b3526783f605cc181c1e12
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-06-26 15:28:43 -0500

    code added to complete moongose promise

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,18 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express() // starting express
+const app = express(); // starting express
+
+const PORT = process.env.PORT || 5000;
+
+
+// Connect to DB and start server
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server Running on port ${PORT}`)
+        })
+    })
+

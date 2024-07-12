7cb0f7fc689dc5409f26820b390071d4329a07c2
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-07-11 19:45:10 -0500

    add username in schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
-
+    name: {
+        type: String,
+        require: [true, "Please add a name"]
+}
 });
 
 const User = mongoose.model("User", userSchema)

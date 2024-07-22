1a24a2b161c48c25cde07db078a920b5f28e2e1c
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-07-21 20:38:47 -0500

    Config errorHandler and registerUser functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,27 @@
-const registerUser = (req, res) => {
-    if (!req.body.email) {
-        res.status(400);
-        throw new Error("Please add an email");
+const asyncHandler = require("express-async-handler")
+const User = require("../models/userModel")
+
+
+const registerUser = asyncHandler( async (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation
+    if (!name || email || password) {
+        res.status(400)
+        throw new Error("please fill all the required fields")
     }
-    res.send("Register user")
-};
+    if (password.lenght < 6) {
+        res.status(400)
+        throw new Error("The password must be up to 6 characters")
+    }
+    
+    // check if the email exists
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.status(400)
+        throw new Error("The email already exists")
+    }
+});
 
 module.exports = {
     registerUser,

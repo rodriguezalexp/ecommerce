8cc2a2910b7647952056b76063d1da5f707d4de8
rodriguezalexp <alexander.0894-rp@hotmail.com>
2024-07-22 13:31:05 -0500

    fix bug in password error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,25 +3,44 @@ const User = require("../models/userModel")
 
 
 const registerUser = asyncHandler( async (req, res) => {
-    const {name, email, password} = req.body
+    const {name, email, password} = req.body;
 
     // Validation
-    if (!name || email || password) {
-        res.status(400)
-        throw new Error("please fill all the required fields")
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("please fill all the required fields");
     }
-    if (password.lenght < 6) {
-        res.status(400)
-        throw new Error("The password must be up to 6 characters")
+    if (password.length < 6) {
+        res.status(400);
+        throw new Error("The password must be up to 6 characters");
     }
     
     // check if the email exists
     const userExists = await User.findOne({email})
     if (userExists) {
-        res.status(400)
-        throw new Error("The email already exists")
+        res.status(400);
+        throw new Error("The email already exists");
+    }
+
+    //Create new user
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if (user) {
+        const {_id, name, email, photo, phone, bio} = user
+        res.status(201).json({
+            _id, name, email, photo, phone, bio,
+        })
+    } else {
+
     }
 });
+
+
 
 module.exports = {
     registerUser,
